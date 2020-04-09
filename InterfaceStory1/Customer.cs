911cbc067959c73467e75850daf7ba45b5fe3ea6
Panos Bozas <fbozas@hotmail.com>
2020-04-09 14:16:32 +0300

    Initial Commit

---
+++ InterfaceStory1/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceStory1
+{
+    class Customer : ICreatable, IDeletable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Create()
+        {
+            Console.WriteLine("The customer with name: " + Name + " has been created");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("The customer with name: " + Name + " has been deleted");
+        }
+    }
+}
---
+++ InterfaceStory1/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceStory1
+{
+    class Product : ICreatable, IDeletable
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public void Create()
+        {
+            Console.WriteLine("The product with name: " + Name + " has been created");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("The product with name: " + Name + " has been deleted");
+        }
+    }
+}
